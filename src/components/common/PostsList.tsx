b5222a3d8e59458e800547fdd2110ae4d5880f5d
Giv <mishin0130@gmail.com>
2026-01-27 18:22:14 +0900

    feat:change text while adding

--- src/components/common/PostsList.tsx
+++ src/components/common/PostsList.tsx
@@ -1,15 +1,15 @@
 import { Post } from "@/lib/actions"
 import { SubmitButton } from "./SubmitButton";
 
-export const PostsList = ({ posts, handleDelete }: { posts: Post[], handleDelete: (formData: FormData) => void }) => {
+export const PostsList = ({ posts, handleDelete }: { posts: (Post | Post & { sending: boolean })[], handleDelete: (formData: FormData) => void }) => {
   return (
     <ul className="space-y-2" >
-      {posts.map((post) => (
+      {posts.map((post: Post | Post & { sending: boolean }) => (
         <li key={post.id} className="flex justify-between gap-4 p-6 bg-gray-100 rounded">
           <p className="text-black p-2">{post.title}</p>
           <form action={handleDelete}>
             <input type="hidden" name="id" value={post.id}></input>
-            <SubmitButton label="削除する" />
+            <SubmitButton label={post.hasOwnProperty('sending') ? "追加中..." : "削除する"} />
           </form>
 
         </li>
